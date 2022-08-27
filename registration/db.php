ac410e4f1c1dc589c8246c44da1e51856fc6ba2d
ibox3667 <ibrohimtlapbayev@gmail.com>
2022-08-27 17:29:01 -0200

    online database
    created

--- registration/db.php
+++ registration/db.php
@@ -1,6 +1,6 @@
 <?php
   
-    $con = mysqli_connect("localhost","root","root","admin_panel");
+    $con = mysqli_connect("sql6.freesqldatabase.com","sql6515245","iLI38q3hwy","sql6515245", 3306);
   
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
